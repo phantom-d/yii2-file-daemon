2054619bfa2786148e00a5d2d8b9a2f134281985
Anton Ermolovich <anton.ermolovich@gmail.com>
2016-02-17 10:32:55 +0300

    master Reconfigure base architectory configuration

--- console/controllers/FileDaemonController.php
+++ console/controllers/FileDaemonController.php
@@ -3,7 +3,7 @@
 namespace phantomd\filedaemon\console\controllers;
 
 use yii\helpers\FileHelper;
-use phantomd\filedaemon\console\controllers\StreakDaemonController;
+use phantomd\filedaemon\common\controllers\StreakDaemonController;
 
 /**
  * Class FileDaemonController. Base file deamon controller for the extension.
